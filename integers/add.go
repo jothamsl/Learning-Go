043b25f34e87ab3d49f0d72228bb7ae2a4ac1e3d
jothamsl <jothamsl1@gmail.com>
2021-09-18 20:58:40 +0100

    Finished add.go

---
+++ integers/add.go
@@ -0,0 +1,9 @@
+package integers
+
+// Adding comments above functions, make them appear in GoDoc
+
+// Add takes two integers and returns their sum
+func Add(x, y int) int { 
+    // same as Add(x int, y int)
+    return x + y
+}
