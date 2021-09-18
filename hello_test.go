b3d5405ae16eb658574120fbd2fea442c28581f6
jothamsl <jothamsl1@gmail.com>
2021-09-18 17:45:25 +0100

    Updated Feature of hello.go

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sam")
-	want := "Hello, Sam"
+    assertCorrectMessage := func(t testing.TB, got, want string) {
+        t.Helper() // if error occurs, error comes from subtest not this helper function 
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Sam")
+        want := "Hello, Sam"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("empty string defaults to 'World'", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
 }
