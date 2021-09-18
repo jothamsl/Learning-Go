c4453b224a10a3c63bfa76185c34c79a60fa831d
jothamsl <jothamsl1@gmail.com>
2021-09-18 18:59:20 +0100

    Finished up Hello.go

--- hello.go
+++ hello.go
@@ -1,16 +1,37 @@
-package main 
+package main
 
 import "fmt"
 
+const yoruba = "yoruba"
+const french = "french"
 const englishHelloPrefix = "Hello, "
+const yorubaHelloPrefix = "Pele o, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
-    if name == "" {
-        name = "World"
-    }
-    return englishHelloPrefix + name
+func Hello(name string, lang string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(lang) + name
+}
+
+// In Go, public functions start with a capital letter and private 
+// ones start with a lowercase
+
+func greetingPrefix(lang string) (prefix string) { // "prefix" is a named return value
+	switch lang {
+	case yoruba:
+        prefix = yorubaHelloPrefix
+	case french:
+        prefix = frenchHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+	}
+    return
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -10,15 +10,27 @@ func TestHello(t *testing.T) {
         }
     }
 
-    t.Run("saying hello to people", func(t *testing.T) {
-        got := Hello("Sam")
+    t.Run("Saying hello to people", func(t *testing.T) {
+        got := Hello("Sam", "")
         want := "Hello, Sam"
         assertCorrectMessage(t, got, want)
     })
 
-    t.Run("empty string defaults to 'World'", func(t *testing.T) {
-        got := Hello("")
+    t.Run("Empty name string defaults to 'World'", func(t *testing.T) {
+        got := Hello("", "")
         want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("Unrecognized language defaults to English", func (t *testing.T) {
+        got := Hello("Mayowa", "German")
+        want := "Hello, Mayowa"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("Check if language of french returns correct output", func (t *testing.T) {
+        got := Hello("Jake", "french")
+        want := "Bonjour, Jake"
         assertCorrectMessage(t, got, want)
     })
 }
