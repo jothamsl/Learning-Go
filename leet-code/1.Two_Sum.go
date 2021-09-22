de6322080916a01dd81150c26a455ef73811fbd8
jothamsl <jothamsl1@gmail.com>
2021-09-22 18:56:46 +0100

    Finished leetcode TwoSum problem

--- leet-code/1.Two_Sum.go
+++ leet-code/1.Two_Sum.go
@@ -1,18 +1,20 @@
 package leetcode
 
-import "fmt"
+func TwoSum(arr []int, target int) (res [2]int) {
 
-func TwoSum(arr []int, val int) (res []int) {
-	arrlength := len(arr)
-	fmt.Println(arrlength)
-
-	for j := arrlength - 1; j > 0; j-- {
-
-		fmt.Println(arr[j])
+	// Loop Through values in array
+	for idx, v := range arr {
+		// Using the value 'v', loop through other values of array:
+		for i := 0; i < len(arr)-1; i++ {
+			// Find the difference between the target and 'v'
+			diff := target - v
+			// If the differece value is the same as the indexed value:
+			if diff == arr[i] {
+				// assign to index of array
+				res[1] = idx
+				res[0] = i
+			}
+		}
 	}
-
-	for i := 0; i < arrlength; i++ {
-		fmt.Println(arr[i])
-	}
-	return
+	return res
 }
